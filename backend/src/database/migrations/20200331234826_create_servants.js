57a78e3f5b67dec0c2585aefa78ae125443d141a
Rafael <rafael-rafa2.1@hotmail.com>
2020-05-20 16:42:16 -0300

    Configurações Heroku 2

--- backend/src/database/migrations/20200331234826_create_servants.js
+++ backend/src/database/migrations/20200331234826_create_servants.js
@@ -1,8 +1,9 @@
 //npx knex migrate:latest
 exports.up = function(knex) {
     return knex.schema.createTable('servants', function (table) {
-        table.increments('id');
-        table.string('user').notNullable();
+        // table.increments('id');
+        // table.string('user').notNullable();
+        table.string('user').primary();
         table.string('password').notNullable();
         table.boolean('passwordTemporary').notNullable();
         table.string('name').notNullable();
--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.24:3333'
+    // baseURL: 'http://192.168.0.24:3333'
+    baseURL: 'https://deapp-backend.herokuapp.com'
 });
 
 export default api;
